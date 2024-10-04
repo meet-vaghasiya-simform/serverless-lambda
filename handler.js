7eee7e7e3216d7f33b34df40fbe21845c188a883
Meet Vaghasiya <meet.vaghasiya@simformsolutions.com>
2024-10-04 21:12:51 +0530

    feat: basic lambda function

--- handler.js
+++ handler.js
@@ -1,74 +1,25 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const logger = require('./logger');
 
-const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} = require("@aws-sdk/lib-dynamodb");
+module.exports.myFunction = async (event) => {
+  const failureChance = Math.random();
+  logger.info('Received request', { event });
 
-const express = require("express");
-const serverless = require("serverless-http");
-
-const app = express();
-
-const USERS_TABLE = process.env.USERS_TABLE;
-const client = new DynamoDBClient();
-const docClient = DynamoDBDocumentClient.from(client);
-
-app.use(express.json());
-
-app.get("/users/:userId", async (req, res) => {
-  const params = {
-    TableName: USERS_TABLE,
-    Key: {
-      userId: req.params.userId,
-    },
-  };
-
-  try {
-    const command = new GetCommand(params);
-    const { Item } = await docClient.send(command);
-    if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
-  }
-});
-
-app.post("/users", async (req, res) => {
-  const { userId, name } = req.body;
-  if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
-
-  const params = {
-    TableName: USERS_TABLE,
-    Item: { userId, name },
-  };
-
-  try {
-    const command = new PutCommand(params);
-    await docClient.send(command);
-    res.json({ userId, name });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Could not create user" });
-  }
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (failureChance < 0.5) {
+        const error = new Error('Random failure occurred');
+        logger.error('Function failed', { error });
+        reject({
+          statusCode: 500,
+          body: JSON.stringify({ message: 'Function failed' }),
+        });
+      } else {
+        logger.info('Function executed successfully');
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Function executed successfully' }),
+        });
+      }
+    }, 10000); // 10 seconds delay
   });
-});
-
-exports.handler = serverless(app);
+};
